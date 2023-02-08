89aadb66c78091a08800b0fb4224b018c40a7550
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2023-02-08 10:01:36 +0100

    Init Spring Boot 3.0 Security

---
+++ src/test/java/de/neuefische/spring30/Spring30ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.spring30;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring30ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
