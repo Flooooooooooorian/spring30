77b80424903e67cd46fb95021c5bf504964ca1be
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2023-02-08 11:15:17 +0100

    Add Basic Integrationtest

---
+++ src/test/java/de/neuefische/spring30/UserControllerTest.java
@@ -0,0 +1,38 @@
+package de.neuefische.spring30;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DirtiesContext
+    @WithMockUser(username = "user", password = "123")
+    void getMe2() throws Exception {
+        //GIVEN
+
+        //WHEN
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/users/me2"))
+        //THEN
+                .andExpect(status().isOk())
+                .andExpect(content().string("user"));
+
+
+    }
+}
