9f410ef82dd3a494ed092ad3c207c4ba5e03e63f
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2023-02-08 10:07:45 +0100

    Add User/Me Controller

--- src/main/java/de/neuefische/spring30/SecurityConfig.java
+++ src/main/java/de/neuefische/spring30/SecurityConfig.java
@@ -2,11 +2,13 @@ package de.neuefische.spring30;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
@@ -26,5 +28,18 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .httpBasic().and()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .and().build();
     }
 }
